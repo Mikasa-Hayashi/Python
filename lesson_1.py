ee5bb0fae02f0531e2d53b1ed12669eab6014ce5
Mikasa-Hayashi <155042885+Mikasa-Hayashi@users.noreply.github.com>
2025-10-18 16:40:58 +0300

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,56 @@
+# Переменная - это по сути именованная "коробка", в которую можно положить
+# данные, чтобы потом использовать их в коде
+
+# тип данных - это характер данных, который определяет, как с ними 
+# можно работать
+# int - целые числа
+# float - вещественные числа
+# str - строки
+# bool - логические значения (True, False)
+
+# int (integer)
+age = 20
+score = 95
+year = 2025
+tempreture = -10
+
+print(age)
+print(score)
+print(year)
+print(tempreture)
+
+
+# float
+pi = 3.14
+height = 255.98
+weight = 76.778
+temp = -12.423
+
+print(pi)
+print(height)
+print(weight)
+print(temp)
+
+# str (string)
+name = 'Ivan'
+greeting = 'Hello!'
+language = 'python'
+
+print(name)
+print(greeting)
+print(language)
+
+# bool
+is_adult = False
+is_raining = True
+
+print(is_adult)
+print(is_raining)
+
+print(10)
+print(1.2)
+print('Hello world')
+print(True)
+
+a, b, c, d = 1, -0.2, 'hi', False
+print(a, b, c, d)
