08eefa3485a5a3b020902ae40d4fd08d2ea65a20
Mikasa-Hayashi <155042885+Mikasa-Hayashi@users.noreply.github.com>
2025-11-05 12:51:10 +0300

    Add files via upload

---
+++ lesson_5.py
@@ -0,0 +1,23 @@
+# Ввод данных — это способ получить информацию от пользователя 
+# во время работы программы.
+
+# Функция для ввода данных - input()
+
+# animal = input()
+# print(f'I love {animal}!')
+
+# name = input('Enter your name: ')
+# age = input('Enter age name: ')
+# print(f'Hello, {name} you\'re {age} years old')
+
+# first_number = input("Enter first number: ") # '2'
+# print(type(first_number))
+# second_number = input("Enter second number: ") # '3'
+# print(first_number + second_number) # '2' + '3' = '23'
+
+# print('2' + '3')
+
+first_number = int(input("Enter first number: "))
+print(type(first_number))
+second_number = int(input("Enter second number: "))
+print(first_number + second_number)
