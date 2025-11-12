b6327f94e92c004d097f3e4ba320efd3591dc005
Mikasa-Hayashi <155042885+Mikasa-Hayashi@users.noreply.github.com>
2025-11-12 16:33:46 +0300

    Add files via upload

---
+++ lesson_7.py
@@ -0,0 +1,48 @@
+# Ветвление - это механизм, который позволяет программе выбирать, 
+# какой код выполнить, в зависимости от условий.
+
+# Проще говоря: "если выполняется X — делаем это, 
+# иначе — делаем другое".
+
+# age = int(input('Enter your age'))
+
+# if age < 0:
+#     print('Incrorrect age (<0)')
+# elif age > 120:
+#     print('Incrorrect age (>120)')
+# print('Smth else')
+
+# mark = -100
+
+# if mark == 5:
+#     print('Excellent')
+# elif mark == 4:
+#     print('Good')
+# elif mark == 3:
+#     print('Satisfied')
+# elif mark == 2:
+#     print('Bad')
+# else:
+#     print('Incorrect mark')
+
+# some code
+
+# Операторы сравнения
+# <
+# >
+# <=
+# >=
+# ==
+# !=
+
+# a = 3
+
+# if a != 3:
+#     print('OK')
+
+num = int(input('Enter some num: '))
+
+if num % 2:
+    print('Odd')
+else:
+    print('Even')
